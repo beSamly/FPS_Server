c815d3a81ffad0b54f9648b0b56ff76e2fad48f6
unknown <sangyeonlee@joycity.com>
2023-02-16 15:41:56 +0900

    Removed unnecessary files from GameServer project

--- GameServer/TransformController.h
+++ GameServer/TransformController.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "Vector3.h"
 
+using PhysicsEngine::Vector3;
+
 class TransformController
 {
 private:
-    Vector3 position;
-    Vector3 targetPosition;
+	Vector3 position;
+	Vector3 targetPosition;
 
 public:
-    TransformController() {};
-    Vector3 GetPosition();
-    Vector3 GetTargetPosition() { return targetPosition; };
-    void SetTargetPosition(Vector3 p_position) { targetPosition = p_position; };
-    void Update(float deltaTime);
-    bool IsMoving();
+	TransformController() {};
+	Vector3 GetPosition();
+	Vector3 GetTargetPosition() { return targetPosition; };
+	void SetTargetPosition(Vector3 p_position) { targetPosition = p_position; };
+	void Update(float deltaTime);
+	bool IsMoving();
 };
--- SharedPacket/SharedPacket/ProxyPacketId.cpp
+++
@@ -1,1 +0,0 @@
-#include "ProxyPacketId.h"
